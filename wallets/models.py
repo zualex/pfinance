ce5ff0435896b83311edc62d5d1aeb48989b967a
zualex <Azubarev32@yandex.ru>
2019-01-18 17:56:55 +0300

    wallet currency foreign key

--- wallets/models.py
+++ wallets/models.py
@@ -2,19 +2,20 @@ from django.conf import settings
 from django.db import models
 
 
-class Wallet(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=70)
-    is_active = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class Currency(models.Model):
+    name = models.CharField(max_length=3)
 
     def __str__(self):
         return self.name
 
 
-class Currency(models.Model):
-    name = models.CharField(max_length=3)
+class Wallet(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    currency = models.ForeignKey('Currency', on_delete=models.CASCADE)
+    name = models.CharField(max_length=70)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
