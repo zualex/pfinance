f5ab164162d1b8d65519b8a67ec4b635f173eb11
zualex <Azubarev32@yandex.ru>
2019-01-28 17:26:38 +0300

    start html template

--- wallets/views.py
+++ wallets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Example'
+    }
+
+    return render(request, 'wallets/index.html', context)
