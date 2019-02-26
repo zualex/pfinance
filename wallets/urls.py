1bbb8a5ac8edcf5c9fcad7b2f3288b59ffd61b43
zualex <Azubarev32@yandex.ru>
2019-02-26 18:00:20 +0300

    update wallets

--- wallets/urls.py
+++ wallets/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'wallets'
 urlpatterns = [
     path('', views.index, name='index'),
     path('wallets', views.wallets, name='wallets'),
     path('wallets/create', views.wallet_create, name='wallet-create'),
+    path('wallets/<int:wallet_id>/update', views.wallet_update, name='wallet-update'),
 ]
--- wallets/views.py
+++ wallets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -34,7 +34,7 @@ def wallet_create(request):
             wallet.user = request.user
             wallet.save()
 
-            return HttpResponseRedirect(reverse('wallets'))
+            return HttpResponseRedirect(reverse('wallets:wallets'))
     else:
         form = WalletForm()
 
@@ -44,3 +44,25 @@ def wallet_create(request):
     }
 
     return render(request, 'wallets/wallet-create.html', context)
+
+
+def wallet_update(request, wallet_id):
+    wallet = get_object_or_404(Wallet, pk=wallet_id)
+    form = WalletForm(instance=wallet)
+
+    if request.method == 'POST':
+        wallet.name = request.POST['name']
+        wallet.balance = request.POST['balance']
+        wallet.currency = Currency.objects.get(pk=request.POST['currency'])
+        wallet.is_active = 'is_active' in request.POST
+        wallet.save()
+
+        return HttpResponseRedirect(reverse('wallets:wallets'))
+
+    context = {
+        'title': 'Update wallet',
+        'id': wallet_id,
+        'form': form,
+    }
+
+    return render(request, 'wallets/wallet-update.html', context)
